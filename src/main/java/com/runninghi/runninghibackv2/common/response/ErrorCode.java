7d71e4fcbb99bfbcfb6abd3d67057361be8eaeca
sui <numerical43@daum.net>
2024-02-27 15:10:36 +0900

    style : 세미콜론 삭제

--- src/main/java/com/runninghi/runninghibackv2/common/response/ErrorCode.java
+++ src/main/java/com/runninghi/runninghibackv2/common/response/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND,"COMMON ERR-404","페이지를 찾을 수 없습니다."),
     INTER_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON ERR-500","해당 ID는 존재하지 않습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON ERR-400", "잘못된 요청입니다.");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON ERR-400", "잘못된 요청입니다.")
     ;
 
     private final HttpStatus status;
