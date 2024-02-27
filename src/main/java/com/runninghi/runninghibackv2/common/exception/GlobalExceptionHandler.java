c8400b9bd8507a6eee0f4b5d3dd05230ab1b41d3
sui <numerical43@daum.net>
2024-02-27 15:45:00 +0900

    fix : ExceptionHandler -> GlobalExceptionHandler로 수정

--- src/main/java/com/runninghi/runninghibackv2/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/runninghi/runninghibackv2/common/exception/GlobalExceptionHandler.java
@@ -4,14 +4,13 @@ import com.runninghi.runninghibackv2.common.response.ApiResult;
 import com.runninghi.runninghibackv2.common.response.ErrorCode;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-
 @RestControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler({NoSuchElementException.class, BadRequestException.class})
+    @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ApiResult> handleBadRequestException() {
         ApiResult apiResult = ApiResult.error(ErrorCode.BAD_REQUEST);
         return ResponseEntity.badRequest().body(apiResult);
