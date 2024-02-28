5f05746238c2ccaceb4370042e3777953c177265
UncleSubak <kko6369@naver.com>
2024-02-28 12:18:37 +0900

    [#10]Feat : 댓글 엔티티 제작

---
+++ src/main/java/com/runninghi/runninghibackv2/comment/domain/aggregate/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.runninghi.runninghibackv2.comment.domain.aggregate.entity;
+
+import com.runninghi.runninghibackv2.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@Table(name = "TBL_COMMENT")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentNo;
+
+//    @ManyToOne
+//    @JoinColumn(name = "MEMBER_KEY")
+//    private Member memberKey;
+
+//    @ManyToOne
+//    @JoinColumn(name = "USERPOST_NO")
+//    private UserPost userPostNo;
+
+    @Column(nullable = false)
+    private String commentContent;
+
+    @Column(nullable = false)
+    private boolean commentStatus;
+
+    @Column
+    private Long parent;
+
+
+
+}
