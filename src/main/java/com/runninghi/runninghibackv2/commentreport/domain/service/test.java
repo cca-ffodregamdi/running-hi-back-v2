e2f0a76135a91466df857d9946d0bc60b34fc50a
sui <numerical43@daum.net>
2024-02-28 23:37:44 +0900

    feat : commentReport entity 설계, 기본 service repository 파일 생성

---
+++ src/main/java/com/runninghi/runninghibackv2/commentreport/domain/service/test.java
@@ -0,0 +1,4 @@
+package com.runninghi.runninghibackv2.commentreport.domain.service;
+
+public class test {
+}
---
+++ src/main/java/com/runninghi/runninghibackv2/postreport/infrastructure/repository/test.java
@@ -0,0 +1,4 @@
+package com.runninghi.runninghibackv2.postreport.infrastructure.repository;
+
+public class test {
+}
