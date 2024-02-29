37a5ff5c31e77c78ac9bfed261ca375435e3caed
sui <numerical43@daum.net>
2024-02-29 21:11:30 +0900

    feat : feedback 저장 기능

--- src/main/java/com/runninghi/runninghibackv2/feedback/application/controller/FeedbackController.java
+++ src/main/java/com/runninghi/runninghibackv2/feedback/application/controller/FeedbackController.java
@@ -1,8 +1,14 @@
 package com.runninghi.runninghibackv2.feedback.application.controller;
 
+import com.runninghi.runninghibackv2.common.response.ApiResult;
+import com.runninghi.runninghibackv2.feedback.application.dto.request.CreateFeedbackRequest;
+import com.runninghi.runninghibackv2.feedback.application.dto.response.CreateFeedbackResponse;
 import com.runninghi.runninghibackv2.feedback.application.service.FeedbackReplyService;
 import com.runninghi.runninghibackv2.feedback.application.service.FeedbackService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +19,15 @@ public class FeedbackController {
     private final FeedbackReplyService feedbackReplyService;
 
     // 피드백 작성
+    @PutMapping
+    public ResponseEntity<ApiResult> createFeedback(@RequestBody CreateFeedbackRequest request){
+
+        Long memberNo = 1L;
+
+        CreateFeedbackResponse response = feedbackService.createFeedback(request, memberNo);
+
+        return ResponseEntity.ok(ApiResult.success("메세지 전송 성공", response));
+    }
 
     // 피드백 조회
 
