7918f6a4bac6c0ed31b9d3efd6236c0f0ef7f90a
sui <numerical43@daum.net>
2024-02-29 21:11:30 +0900

    feat : feedback 저장 기능

--- src/main/java/com/runninghi/runninghibackv2/feedback/application/dto/response/CreateFeedbackResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/feedback/application/dto/response/CreateFeedbackResponse.java
@@ -1,4 +1,11 @@
 package com.runninghi.runninghibackv2.feedback.application.dto.response;
 
-public class CreateFeedbackResponse {
+public record CreateFeedbackResponse(
+        Long feedbackNo,
+        String title,
+        String content
+) {
+    public static CreateFeedbackResponse create(Long feedbackNo, String title, String content) {
+        return new CreateFeedbackResponse(feedbackNo, title, content);
+    }
 }
--- src/main/java/com/runninghi/runninghibackv2/feedback/application/service/FeedbackService.java
+++ src/main/java/com/runninghi/runninghibackv2/feedback/application/service/FeedbackService.java
@@ -1,9 +1,43 @@
 package com.runninghi.runninghibackv2.feedback.application.service;
 
+import com.runninghi.runninghibackv2.feedback.application.dto.request.CreateFeedbackRequest;
+import com.runninghi.runninghibackv2.feedback.application.dto.response.CreateFeedbackResponse;
+import com.runninghi.runninghibackv2.feedback.domain.aggregate.entity.Feedback;
+import com.runninghi.runninghibackv2.feedback.domain.aggregate.entity.FeedbackCategory;
+import com.runninghi.runninghibackv2.feedback.domain.repository.FeedbackRepository;
+import com.runninghi.runninghibackv2.feedback.domain.service.FeedbackDomainService;
+import com.runninghi.runninghibackv2.member.domain.aggregate.entity.Member;
+import com.runninghi.runninghibackv2.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class FeedbackService {
+
+    private final MemberRepository memberRepository;
+    private final FeedbackRepository feedbackRepository;
+    private final FeedbackDomainService feedbackDomainService;
+
+    @Transactional
+    public CreateFeedbackResponse createFeedback(CreateFeedbackRequest request, Long memberNo) {
+
+        Member member = memberRepository.findById(memberNo)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid memberNo"));
+
+        feedbackDomainService.checkFeedbackValidation(request.title(), request.content());
+
+        Feedback feedback = new Feedback.Builder()
+                .feedbackWriter(member)
+                .title(request.title())
+                .content(request.content())
+                .category(FeedbackCategory.getFeedbackCategoryFromValue(request.category()))
+                .hasReply(false)
+                .builder();
+
+        feedbackRepository.save(feedback);
+
+        return CreateFeedbackResponse.create(feedback.getFeedbackNo(), feedback.getTitle(), feedback.getContent());
+    }
 }
