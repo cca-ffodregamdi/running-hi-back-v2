c2ea77bf85f2793c8dfc7be2137906a0dfdae8b4
UncleSubak <kko6369@naver.com>
2024-03-01 20:38:45 +0900

    [#10]feat : 북마크 폴더 엔티티 생성

---
+++ src/main/java/com/runninghi/runninghibackv2/bookmarkfolder/domain/aggregate/entity/BookmarkFolder.java
@@ -0,0 +1,63 @@
+package com.runninghi.runninghibackv2.bookmarkfolder.domain.aggregate.entity;
+
+import com.runninghi.runninghibackv2.member.domain.aggregate.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "TBL_BOOKMARK_FOLDER")
+public class BookmarkFolder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookmarkFolderNo;
+
+    @Column(nullable = false, length = 30)
+    private String folderName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_NO")
+    private Member member;
+
+    private BookmarkFolder(BookmarkFolderBuilder builder) {
+        this.bookmarkFolderNo = builder.bookmarkFolderNo;
+        this.folderName = builder.folderName;
+        this.member = builder.member;
+    }
+
+    public static class BookmarkFolderBuilder {
+
+        private Long bookmarkFolderNo;
+        private String folderName;
+        private Member member;
+
+        public static BookmarkFolderBuilder builder() {
+            return new BookmarkFolderBuilder();
+        }
+
+        public BookmarkFolderBuilder bookmarkFolderNo(Long bookmarkFolderNo) {
+            this.bookmarkFolderNo = bookmarkFolderNo;
+            return this;
+        }
+
+        public BookmarkFolderBuilder folderName(String folderName) {
+            this.folderName = folderName;
+            return this;
+        }
+
+        public BookmarkFolderBuilder member(Member member) {
+            this.member = member;
+            return this;
+        }
+
+        public BookmarkFolder build() {
+            return new BookmarkFolder(this);
+        }
+
+    }
+
+}
