e267d060dc74aa0abb0ef5d415ca5a888858cbe4
silverpoodle <jungin.dylan@gmail.com>
2024-03-07 15:26:58 +0900

    feat: Keyword 예외처리

--- src/main/java/com/runninghi/runninghibackv2/keyword/domain/repository/KeywordDomainRepository.java
+++ src/main/java/com/runninghi/runninghibackv2/keyword/domain/repository/KeywordDomainRepository.java
@@ -5,6 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface KeywordDomainRepository extends JpaRepository<Keyword, Long> {
+
+    Optional<Keyword> findByKeywordName(String keywordName);
+
 }
--- src/main/java/com/runninghi/runninghibackv2/keyword/infrastructure/service/test.java
+++
@@ -1,4 +0,0 @@
-package com.runninghi.runninghibackv2.keyword.infrastructure.service;
-
-public class test {
-}
