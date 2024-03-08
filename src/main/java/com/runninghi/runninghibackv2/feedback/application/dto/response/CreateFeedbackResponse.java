c198397ae868b169f06ded836e5a1825fee85e1a
sui <numerical43@daum.net>
2024-03-08 15:36:07 +0900

    refactor : dto의 필드값을 내부에서 생성하도록 수정

--- src/main/java/com/runninghi/runninghibackv2/feedback/application/dto/response/CreateFeedbackResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/feedback/application/dto/response/CreateFeedbackResponse.java
@@ -1,11 +1,13 @@
 package com.runninghi.runninghibackv2.feedback.application.dto.response;
 
+import com.runninghi.runninghibackv2.feedback.domain.aggregate.entity.Feedback;
+
 public record CreateFeedbackResponse(
         Long feedbackNo,
         String title,
         String content
 ) {
-    public static CreateFeedbackResponse create(Long feedbackNo, String title, String content) {
-        return new CreateFeedbackResponse(feedbackNo, title, content);
+    public static CreateFeedbackResponse create(Feedback feedback) {
+        return new CreateFeedbackResponse(feedback.getFeedbackNo(), feedback.getTitle(), feedback.getContent());
     }
 }
--- src/main/java/com/runninghi/runninghibackv2/feedback/application/dto/response/UpdateFeedbackReplyResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/feedback/application/dto/response/UpdateFeedbackReplyResponse.java
@@ -1,5 +1,6 @@
 package com.runninghi.runninghibackv2.feedback.application.dto.response;
 
+import com.runninghi.runninghibackv2.feedback.domain.aggregate.entity.Feedback;
 import com.runninghi.runninghibackv2.feedback.domain.aggregate.entity.FeedbackCategory;
 
 import java.time.LocalDateTime;
@@ -14,9 +15,9 @@ public record UpdateFeedbackReplyResponse(
         String reply,
         String nickname
 ) {
-    public static UpdateFeedbackReplyResponse create(String title, String content, FeedbackCategory category,
-                                             LocalDateTime createDate, LocalDateTime updateDate, boolean hasReply,
-                                             String reply, String nickname) {
-        return new UpdateFeedbackReplyResponse(title, content, category, createDate, updateDate, hasReply, reply, nickname);
+    public static UpdateFeedbackReplyResponse create(Feedback feedback) {
+        return new UpdateFeedbackReplyResponse(feedback.getTitle(), feedback.getContent(),
+                feedback.getCategory(), feedback.getCreateDate(), feedback.getUpdateDate(),
+                feedback.isHasReply(), feedback.getReply(), feedback.getFeedbackWriter().getNickname());
     }
 }
