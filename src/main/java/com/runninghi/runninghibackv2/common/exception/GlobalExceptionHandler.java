bd2b7dce3189f12cd226224b5f91626661b4aafa
UncleSubak <kko6369@naver.com>
2024-03-11 17:18:40 +0900

    [#27]Refactor : 2024.03.11 - delete 예외처리 GlobalExceptionHandler로 옮겼습니다.

--- src/main/java/com/runninghi/runninghibackv2/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/runninghi/runninghibackv2/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.runninghi.runninghibackv2.common.exception;
 import com.runninghi.runninghibackv2.common.response.ApiResult;
 import com.runninghi.runninghibackv2.common.response.ErrorCode;
 import org.apache.coyote.BadRequestException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +15,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResult> handleBadRequestException() {
         ApiResult apiResult = ApiResult.error(ErrorCode.BAD_REQUEST);
         return ResponseEntity.badRequest().body(apiResult);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ApiResult> handleEmptyResultDataAccessException() {
+        return ResponseEntity.badRequest().body(ApiResult.error(ErrorCode.BAD_REQUEST));
     }
 
 }
