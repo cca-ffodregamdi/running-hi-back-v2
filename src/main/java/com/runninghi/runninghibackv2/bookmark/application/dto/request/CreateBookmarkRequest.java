7d477dae7c6d37919fa7c8d84916bf56e209c845
UncleSubak <kko6369@naver.com>
2024-03-11 16:16:07 +0900

    [#27]Feat : 2024.03.11 - 북마크 crd 기능 개발

---
+++ src/main/java/com/runninghi/runninghibackv2/bookmark/application/dto/request/CreateBookmarkRequest.java
@@ -0,0 +1,7 @@
+package com.runninghi.runninghibackv2.bookmark.application.dto.request;
+
+public record CreateBookmarkRequest(
+        Long memberNo,
+        Long postNo
+) {
+}
---
+++ src/main/java/com/runninghi/runninghibackv2/bookmark/application/service/BookmarkService.java
@@ -0,0 +1,68 @@
+package com.runninghi.runninghibackv2.bookmark.application.service;
+
+import com.runninghi.runninghibackv2.bookmark.application.dto.request.CreateBookmarkRequest;
+import com.runninghi.runninghibackv2.bookmark.application.dto.response.BookmarkedPostListResponse;
+import com.runninghi.runninghibackv2.bookmark.application.dto.response.CreateBookmarkResponse;
+import com.runninghi.runninghibackv2.bookmark.domain.aggregate.entity.Bookmark;
+import com.runninghi.runninghibackv2.bookmark.domain.aggregate.vo.BookmarkId;
+import com.runninghi.runninghibackv2.bookmark.domain.repository.BookmarkRepository;
+import com.runninghi.runninghibackv2.bookmark.domain.service.ApiBookmarkService;
+import com.runninghi.runninghibackv2.member.domain.aggregate.entity.Member;
+import com.runninghi.runninghibackv2.post.domain.aggregate.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private ApiBookmarkService apiBookmarkService;
+
+
+    @Transactional(readOnly = true)
+    public  List<BookmarkedPostListResponse> getBookmarkedPostList(Long memberNo) {
+        List<Bookmark> bookmarkListResult = bookmarkRepository.findAllByMemberNo(memberNo)
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 항목이 없습니다."));
+
+        List<BookmarkedPostListResponse> bookmarkList =
+                bookmarkListResult.stream().map(i -> BookmarkedPostListResponse.convertToDTO(i.getPost())).toList();
+        return bookmarkList;
+    }
+
+    @Transactional
+    public CreateBookmarkResponse createBookmark(CreateBookmarkRequest request) {
+
+        BookmarkId bookmarkId = BookmarkId.builder()
+                                        .memberNo(request.memberNo())
+                                        .postNo(request.postNo())
+                                        .build();
+
+        Member member = apiBookmarkService.getMemberById(request.memberNo());
+        Post post = apiBookmarkService.getPostById(request.postNo());
+
+
+        Bookmark bookmark = Bookmark.builder()
+                                .bookmarkId(bookmarkId)
+                                .member(member)
+                                .post(post)
+                                .build();
+
+        return CreateBookmarkResponse.convertToDTO(bookmarkRepository.save(bookmark));
+    }
+
+    @Transactional
+    public void deleteBookmark(Long memberNo, Long postNo) {
+
+        bookmarkRepository.deleteById(
+                BookmarkId.builder()
+                        .memberNo(memberNo)
+                        .postNo(postNo)
+                        .build()
+        );
+    }
+}
