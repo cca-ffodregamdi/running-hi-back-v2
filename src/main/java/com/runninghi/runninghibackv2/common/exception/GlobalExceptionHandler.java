d426c10c4603334ffa75414bf00b1aff728e5d42
sui <numerical43@daum.net>
2024-03-12 16:04:57 +0900

    feat : ErrorCode, Exception 추가

--- src/main/java/com/runninghi/runninghibackv2/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/runninghi/runninghibackv2/common/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.runninghi.runninghibackv2.common.exception;
 
 import com.runninghi.runninghibackv2.common.response.ApiResult;
 import com.runninghi.runninghibackv2.common.response.ErrorCode;
+import jakarta.persistence.EntityNotFoundException;
 import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,4 +19,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(apiResult);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResult> handleAccessDeniedException() {
+        ApiResult apiResult = ApiResult.error(ErrorCode.ACCESS_DENIED);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiResult);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiResult> handleEntityNotFoundException() {
+        ApiResult apiResult = ApiResult.error(ErrorCode.ENTITY_NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiResult);
+    }
 }
--- src/main/java/com/runninghi/runninghibackv2/common/response/ErrorCode.java
+++ src/main/java/com/runninghi/runninghibackv2/common/response/ErrorCode.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND,"COMMON ERR-404","페이지를 찾을 수 없습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND,"COMMON ERR-404-PAGE","페이지를 찾을 수 없습니다."),
     INTER_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON ERR-500","해당 ID는 존재하지 않습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON ERR-400", "잘못된 요청입니다.")
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON ERR-400", "잘못된 요청입니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "COMMON ERR-403", "권한이 없습니다."),
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON ERR-404-ENTITY", "해당 ID에 대한 엔티티를 찾을 수 없습니다.")
     ;
 
     private final HttpStatus status;
