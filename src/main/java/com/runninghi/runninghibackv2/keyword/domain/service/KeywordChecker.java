87392dcdc390ac7698534f776104dac801a8572c
Jungin <jungin.dylan@gmail.com>
2024-03-13 16:18:35 +0900

    fix: Checker 구조 수정

--- src/main/java/com/runninghi/runninghibackv2/keyword/domain/service/KeywordChecker.java
+++ src/main/java/com/runninghi/runninghibackv2/keyword/domain/service/KeywordChecker.java
@@ -3,8 +3,10 @@ package com.runninghi.runninghibackv2.keyword.domain.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface KeywordChecker {
-
-    void checkIsEmpty(String keywordName);
+public class KeywordChecker {
+    public void checkIsEmpty(String keywordName) {
+        if (keywordName.isEmpty() || keywordName.isBlank())
+            throw new IllegalArgumentException("키워드는 공백일 수 없습니다.");
+    }
 
 }
