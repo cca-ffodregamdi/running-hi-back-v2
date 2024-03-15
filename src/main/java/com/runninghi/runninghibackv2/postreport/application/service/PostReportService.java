f012c8c1d0caf63e7ca654e5d8757817b54dbaf1
hodin <hodin030@gmail.com>
2024-03-15 23:53:11 +0900

    feat: PostReport 저장 Service 기능 추가

--- src/main/java/com/runninghi/runninghibackv2/postreport/application/service/PostReportService.java
+++ src/main/java/com/runninghi/runninghibackv2/postreport/application/service/PostReportService.java
@@ -1,10 +1,31 @@
 package com.runninghi.runninghibackv2.postreport.application.service;
 
 
+import com.runninghi.runninghibackv2.common.enumtype.ReportCategory;
+import com.runninghi.runninghibackv2.postreport.application.dto.request.CreatePostReportRequest;
+import com.runninghi.runninghibackv2.postreport.domain.aggregate.entity.PostReport;
+import com.runninghi.runninghibackv2.postreport.domain.repository.PostReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostReportService {
+
+    private PostReportRepository postReportRepository;
+
+    public void createPostReport(CreatePostReportRequest request) {
+
+        PostReport postReport = new PostReport.Builder()
+                .category(ReportCategory.INPROGRESS)
+                .content(request.content())
+                .reportedPostDeleted(false)
+                .reporter(request.reporter())
+                .reportedMember(request.reportedMember())
+                .reportedPost(request.reportedPost())
+                .build();
+
+        postReportRepository.save(postReport);
+
+    }
 }
