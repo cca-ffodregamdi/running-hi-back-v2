8b0069f58eee1ef8a49b294fabf3cf53f97edc40
hodin <hodin030@gmail.com>
2024-03-17 14:46:47 +0900

    refactor: PostReport 저장 Service 기능
    -1. 예외처리 추가
    -2. 리턴값 변경(저장된 PostReport 반환)

--- src/main/java/com/runninghi/runninghibackv2/postreport/application/service/PostReportService.java
+++ src/main/java/com/runninghi/runninghibackv2/postreport/application/service/PostReportService.java
@@ -1,10 +1,12 @@
 package com.runninghi.runninghibackv2.postreport.application.service;
 
-
-import com.runninghi.runninghibackv2.common.enumtype.ReportCategory;
+import com.runninghi.runninghibackv2.common.enumtype.ProcessingStatus;
+import com.runninghi.runninghibackv2.member.domain.aggregate.entity.Member;
 import com.runninghi.runninghibackv2.postreport.application.dto.request.CreatePostReportRequest;
+import com.runninghi.runninghibackv2.postreport.application.dto.response.CreatePostReportResponse;
 import com.runninghi.runninghibackv2.postreport.domain.aggregate.entity.PostReport;
 import com.runninghi.runninghibackv2.postreport.domain.repository.PostReportRepository;
+import com.runninghi.runninghibackv2.postreport.domain.service.PostReportChecker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +14,32 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PostReportService {
 
-    private PostReportRepository postReportRepository;
+    private final PostReportRepository postReportRepository;
+    private final PostReportChecker postReportChecker;
 
-    public void createPostReport(CreatePostReportRequest request) {
+    // 게시글 신고 저장
+    public CreatePostReportResponse createPostReport(CreatePostReportRequest request, Member reporter) {
 
-        PostReport postReport = new PostReport.Builder()
-                .category(ReportCategory.INPROGRESS)
-                .content(request.content())
-                .reportedPostDeleted(false)
-                .reporter(request.reporter())
-                .reportedMember(request.reportedMember())
-                .reportedPost(request.reportedPost())
-                .build();
+        try {
+            postReportChecker.checkPostReportValidation(request, reporter);
 
-        postReportRepository.save(postReport);
+            PostReport postReport = new PostReport.Builder()
+                    .category(request.category())
+                    .content(request.content())
+                    .status(ProcessingStatus.INPROGRESS)
+                    .reportedPostDeleted(false)
+                    .reporter(reporter)
+                    .reportedMember(request.reportedMember())
+                    .reportedPost(request.reportedPost())
+                    .build();
 
+            PostReport insertedPostReport = postReportRepository.save(postReport);
+
+            return CreatePostReportResponse.from(insertedPostReport);
+        }
+        catch (Exception ex) {
+            //TODO. log 추가 후 log 남기도록 리팩토링
+            throw ex;
+        }
     }
 }
