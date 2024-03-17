bb8dd50e9bf3ac01d8ee2cdd38c92ad83ecd5be9
hodin <hodin030@gmail.com>
2024-03-17 14:38:35 +0900

    refactor: 신고 카테고리 enum 변경

--- src/main/java/com/runninghi/runninghibackv2/common/enumtype/ReportCategory.java
+++ src/main/java/com/runninghi/runninghibackv2/common/enumtype/ReportCategory.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ReportCategory {
 
-    INPROGRESS(0, "처리중"),
-    DELETED(1, "삭제됨"),
-    ;
+    SPAM(0, "광고/도배"),
+    ILLEGALITY(1, "불법"),
+    PORNOGRAPHY(2, "음란물"),
+    PROFANITY(3, "욕설/혐오"),
+    OTHER(4, "기타");
 
     private final int value;
     private final String description;
@@ -19,10 +21,12 @@ public enum ReportCategory {
 
     public static String getDescriptionFromValue(int value) {
         return switch (value) {
-            case 0 -> ReportCategory.INPROGRESS.getDescription();
-            case 1 -> ReportCategory.DELETED.getDescription();
+            case 0 -> ReportCategory.SPAM.getDescription();
+            case 1 -> ReportCategory.ILLEGALITY.getDescription();
+            case 2 -> ReportCategory.PORNOGRAPHY.getDescription();
+            case 3 -> ReportCategory.PROFANITY.getDescription();
+            case 4 -> ReportCategory.OTHER.getDescription();
             default -> throw new IllegalArgumentException("카테고리 번호가 올바르지 않습니다.");
         };
     }
-
 }
