75d880348a56bfa76d1f8d697148b59e2fa3c889
UncleSubak <kko6369@naver.com>
2024-03-19 13:59:42 +0900

    [#77] Comment : 2024.03.19 서비스 계층 메소드 문서 주석 추가

--- src/main/java/com/runninghi/runninghibackv2/bookmark/application/service/BookmarkService.java
+++ src/main/java/com/runninghi/runninghibackv2/bookmark/application/service/BookmarkService.java
@@ -9,6 +9,7 @@ import com.runninghi.runninghibackv2.bookmark.domain.repository.BookmarkReposito
 import com.runninghi.runninghibackv2.bookmark.domain.service.ApiBookmarkService;
 import com.runninghi.runninghibackv2.member.domain.aggregate.entity.Member;
 import com.runninghi.runninghibackv2.post.domain.aggregate.entity.Post;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -24,16 +25,26 @@ public class BookmarkService {
     private ApiBookmarkService apiBookmarkService;
 
 
+    /**
+     * 북마크 탭 클릭 시 해당 유저의 북마크 게시글 리스트가 조회되는 메소드
+     * @param memberNo
+     * @return 리스트 형태의 게시글 정보, 기록
+     */
     @Transactional(readOnly = true)
     public  List<BookmarkedPostListResponse> getBookmarkedPostList(Long memberNo) {
-        List<Bookmark> bookmarkListResult = bookmarkRepository.findAllByBookmarkId_MemberNo(memberNo)
-                .orElseThrow(() -> new IllegalArgumentException("일치하는 항목이 없습니다."));
 
-        List<BookmarkedPostListResponse> bookmarkList =
-                bookmarkListResult.stream().map(i -> BookmarkedPostListResponse.fromEntity(i.getPost())).toList();
-        return bookmarkList;
+        List<Bookmark> bookmarkListResult = bookmarkRepository.findAllByBookmarkId_MemberNo(memberNo);
+        if (bookmarkListResult.isEmpty()) throw new EntityNotFoundException("일치하는 항목이 없습니다.");
+
+        return bookmarkListResult.stream().map(i -> BookmarkedPostListResponse.fromEntity(i.getPost())).toList();
+
     }
 
+    /**
+     * 게시글 북마크 추가 메소드
+     * @param request(memberNo, postNo)
+     * @return CreateBookmarkResponse(memberNo, postNo)
+     */
     @Transactional
     public CreateBookmarkResponse createBookmark(CreateBookmarkRequest request) {
 
@@ -55,6 +66,11 @@ public class BookmarkService {
         return CreateBookmarkResponse.fromEntity(bookmarkRepository.save(bookmark));
     }
 
+    /**
+     * 북마크 취소 메소드
+     * @param memberNo
+     * @param postNo
+     */
     @Transactional
     public void deleteBookmark(Long memberNo, Long postNo) {
 
@@ -64,5 +80,6 @@ public class BookmarkService {
                         .postNo(postNo)
                         .build()
         );
+
     }
 }
