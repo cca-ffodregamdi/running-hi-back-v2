9ce013c7cbfc738ef2545fd186142e5f61762743
hodin <hodin030@gmail.com>
2024-03-20 06:46:44 +0900

    feat: PostReport 조회 응답 DTO 생성

---
+++ src/main/java/com/runninghi/runninghibackv2/postreport/application/dto/response/GetPostReportResponse.java
@@ -0,0 +1,31 @@
+package com.runninghi.runninghibackv2.postreport.application.dto.response;
+
+import com.runninghi.runninghibackv2.common.enumtype.ProcessingStatus;
+import com.runninghi.runninghibackv2.common.enumtype.ReportCategory;
+import com.runninghi.runninghibackv2.member.domain.aggregate.entity.Member;
+import com.runninghi.runninghibackv2.post.domain.aggregate.entity.Post;
+import com.runninghi.runninghibackv2.postreport.domain.aggregate.entity.PostReport;
+
+public record GetPostReportResponse(
+        Long postReportNo,
+        ReportCategory category,
+        String content,
+        ProcessingStatus status,
+        boolean reportedPostDeleted,
+        Member reporter,
+        Member reportedMember,
+        Post reportedPost
+) {
+    public static GetPostReportResponse from(PostReport postReport) {
+
+        return new GetPostReportResponse(
+                postReport.getPostReportNo(),
+                postReport.getCategory(),
+                postReport.getContent(),
+                postReport.getStatus(),
+                postReport.isReportedPostDeleted(),
+                postReport.getReporter(),
+                postReport.getReportedMember(),
+                postReport.getReportedPost());
+    }
+}
