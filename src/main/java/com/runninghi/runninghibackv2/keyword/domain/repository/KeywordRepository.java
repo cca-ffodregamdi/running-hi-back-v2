24e17f2de786bdd3cc0c32d61a4f5f874bf1ed29
Jungin <jungin.dylan@gmail.com>
2024-03-20 17:08:22 +0900

    fix: 명칭 수정

--- src/main/java/com/runninghi/runninghibackv2/keyword/domain/repository/KeywordRepository.java
+++ src/main/java/com/runninghi/runninghibackv2/keyword/domain/repository/KeywordRepository.java
@@ -1,14 +1,13 @@
 package com.runninghi.runninghibackv2.keyword.domain.repository;
 
 import com.runninghi.runninghibackv2.keyword.domain.aggregate.entity.Keyword;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface KeywordDomainRepository extends JpaRepository<Keyword, Long> {
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
     Optional<Keyword> findByKeywordName(String keywordName);
 
