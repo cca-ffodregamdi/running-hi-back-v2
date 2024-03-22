1db99fd6f44c83967ed89ba1136f5c51f2199e29
SUI <numerical43@daum.net>
2024-03-22 13:50:44 +0900

    feat : 카카오톡 통신을 위한 restTemplate 빈 설정

--- src/main/java/com/runninghi/runninghibackv2/config/WebConfig.java
+++ src/main/java/com/runninghi/runninghibackv2/config/WebConfig.java
@@ -3,10 +3,18 @@ package com.runninghi.runninghibackv2.config;
 import com.runninghi.runninghibackv2.auth.jwt.JwtTokenProvider;
 import com.runninghi.runninghibackv2.common.filter.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,5 +29,15 @@ public class WebConfig {
         registrationBean.setFilter(new JwtTokenFilter(jwtTokenProvider));
         registrationBean.addUrlPatterns("/api/*");
         return registrationBean;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .setConnectTimeout(Duration.ofMillis(5000)) // 연결 시간
+                .setReadTimeout(Duration.ofMillis(5000)) // 읽기 시간
+                .additionalMessageConverters(new StringHttpMessageConverter(StandardCharsets.UTF_8))
+                .build();
     }
 }
