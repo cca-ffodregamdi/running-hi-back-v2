9fb5c2c15884283d2a58d5b8d33c6dc2b835b508
UncleSubak <kko6369@naver.com>
2024-03-22 13:53:39 +0900

    [#101] Refactor : 2024.03.22 HttpServlet -> RequestHeader로 변경해주었습니다.

--- src/main/java/com/runninghi/runninghibackv2/bookmark/application/controller/BookmarkController.java
+++ src/main/java/com/runninghi/runninghibackv2/bookmark/application/controller/BookmarkController.java
@@ -7,7 +7,6 @@ import com.runninghi.runninghibackv2.bookmark.application.dto.response.CreateBoo
 import com.runninghi.runninghibackv2.bookmark.application.service.BookmarkService;
 import com.runninghi.runninghibackv2.common.dto.MemberJwtInfo;
 import com.runninghi.runninghibackv2.common.response.ApiResult;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(name = "api/v1/bookmark")
+@RequestMapping(value = "/api/v1/bookmark")
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
@@ -26,34 +25,34 @@ public class BookmarkController {
     /**
      * 북마크 탭 클릭 시 요청한 사용자의 북마크된 Post들을 응답해주는 메소드입니다.
      * -> 만약 다른 회원의 북마크 리스트를 보게 한다면 리팩토링 필요
-     * @param servletRequest HttpServletRequest
+     * @param bearerToken HttpServletRequest Header에 담긴 Authorization
      * @return Bookmark 테이블에서 조회된 Posts
      */
     @GetMapping()
-    public ResponseEntity<ApiResult> getBookmarkedPostList(HttpServletRequest servletRequest) {
+    public ResponseEntity<ApiResult> getBookmarkedPostList(@RequestHeader(name = "Authorization") String bearerToken) {
 
-        MemberJwtInfo memberInfo = jwtTokenProvider.getMemberNoAndRoleFromRequest(servletRequest);
+        MemberJwtInfo memberInfo = jwtTokenProvider.getMemberInfoByBearerToken(bearerToken);
         List<BookmarkedPostListResponse> bookmarkedPostList = bookmarkService.getBookmarkedPostList(memberInfo.memberNo());
 
         return ResponseEntity.ok().body(ApiResult.success("성공적으로 조회되었습니다.", bookmarkedPostList));
     }
 
     @PostMapping()
-    public ResponseEntity<ApiResult> createBookmark (HttpServletRequest servletRequest,
+    public ResponseEntity<ApiResult> createBookmark (@RequestHeader("Authorization") String bearerToken,
                                                     @RequestBody(required = true) Long postNo) {
 
-        MemberJwtInfo memberJwtInfo = jwtTokenProvider.getMemberNoAndRoleFromRequest(servletRequest);
+        MemberJwtInfo memberJwtInfo = jwtTokenProvider.getMemberInfoByBearerToken(bearerToken);
         CreateBookmarkRequest request = CreateBookmarkRequest.of(memberJwtInfo.memberNo(), postNo);
         CreateBookmarkResponse response = bookmarkService.createBookmark(request);
 
         return ResponseEntity.ok().body(ApiResult.success("성공적으로 저장되었습니다.", response));
     }
 
-    @DeleteMapping("/{postNo}")
-    public ResponseEntity<ApiResult> deleteBookmark (HttpServletRequest servletRequest,
+    @DeleteMapping(value = "/{postNo}")
+    public ResponseEntity<ApiResult> deleteBookmark (@RequestHeader("Authorization") String bearerToken,
                                                      @PathVariable(name = "postNo") Long postNo) {
 
-            MemberJwtInfo memberJwtInfo = jwtTokenProvider.getMemberNoAndRoleFromRequest(servletRequest);
+            MemberJwtInfo memberJwtInfo = jwtTokenProvider.getMemberInfoByBearerToken(bearerToken);
             bookmarkService.deleteBookmark(memberJwtInfo.memberNo(), postNo);
 
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResult.success("성공적으로 삭제되었습니다.", null)); // statusCode 204
