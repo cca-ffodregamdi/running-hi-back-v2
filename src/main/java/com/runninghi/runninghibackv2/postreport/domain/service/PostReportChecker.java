211ab01c1d76c3aedeffa59ea5792ff5096e3843
hodin <hodin030@gmail.com>
2024-03-24 23:40:31 +0900

    feat: 기타 신고 사유 null 검증

--- src/main/java/com/runninghi/runninghibackv2/postreport/domain/service/PostReportChecker.java
+++ src/main/java/com/runninghi/runninghibackv2/postreport/domain/service/PostReportChecker.java
@@ -1,6 +1,6 @@
 package com.runninghi.runninghibackv2.postreport.domain.service;
 
-import com.runninghi.runninghibackv2.member.domain.aggregate.entity.Member;
+import com.runninghi.runninghibackv2.common.enumtype.ReportCategory;
 import com.runninghi.runninghibackv2.postreport.application.dto.request.CreatePostReportRequest;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +10,23 @@ public class PostReportChecker {
     public void checkPostReportValidation(CreatePostReportRequest request) {
 
         if(request.category() == null) {
-            throw new IllegalArgumentException("저장: 신고 유형은 필수 선택입니다.");
+            throw new IllegalArgumentException("게시글 신고 저장: 신고 유형이 선택되지 않았습니다.");
         }
 
-        if(request.content().length() == 0) {
-            // TODO. 신고 유형이 '기타'인 경우 기타 신고사유 null 검증
+        if(request.category() == ReportCategory.OTHER && request.content().length() == 0) {
+            throw new IllegalArgumentException("게시글 신고 저장: 기타 신고 사유가 입력되지 않았습니다.");
         }
 
         if(request.content().length() > 100) {
-            throw new IllegalArgumentException("저장: 기타 신고사유는 100자를 넘을 수 없습니다.");
+            throw new IllegalArgumentException("게시글 신고 저장: 기타 신고 사유는 100자를 넘을 수 없습니다.");
         }
 
         if(request.reportedMemberNo() == null) {
-            throw new IllegalArgumentException("저장: 피신고자 정보가 없습니다.");
+            throw new IllegalArgumentException("게시글 신고 저장: 피신고자 정보가 없습니다.");
         }
 
         if(request.reportedPostNo() == null) {
-            throw new IllegalArgumentException("저장: 신고된 게시글 정보가 없습니다.");
+            throw new IllegalArgumentException("게시글 신고 저장: 신고된 게시글 정보가 없습니다.");
         }
     }
 }
