1411ba94dd2f0f9dded23cc478b60c7d188aace1
SUI <numerical43@daum.net>
2024-03-25 14:48:36 +0900

    feat : 카카오 로그인 페이지로 바로 리다이렉트 하도록 수정, 경로 수정, Bearer 토큰 수정

--- src/main/java/com/runninghi/runninghibackv2/member/application/controller/MemberController.java
+++ src/main/java/com/runninghi/runninghibackv2/member/application/controller/MemberController.java
@@ -5,11 +5,13 @@ import com.runninghi.runninghibackv2.member.application.service.KakaoLoginServic
 import com.runninghi.runninghibackv2.member.application.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.Map;
 
 @RestController
@@ -23,14 +25,14 @@ public class MemberController {
     /**
      * 카카오 로그인 요청을 처리하는 API 엔드포인트입니다.
      *
-     * @return 카카오 로그인 페이지의 URI
+     * @return 카카오 로그인 페이지로 리다이렉트
      */
     @GetMapping("/api/v1/login/kakao")
-    public ResponseEntity<ApiResult> kakaoLogin() {
+    public ResponseEntity<Void> kakaoLogin() {
 
-        String kakaoUri = kakaoLoginService.getKakaoUri();
+        URI kakaoUri = URI.create(kakaoLoginService.getKakaoUri());
 
-        return ResponseEntity.ok(ApiResult.success("Redirect Success", kakaoUri));
+        return ResponseEntity.status(HttpStatus.FOUND).location(kakaoUri).build();
     }
 
     /**
@@ -39,13 +41,13 @@ public class MemberController {
      * @param code 카카오로부터 받은 인가 코드
      * @return 로그인 성공 여부 및 인증 토큰 정보
      */
-    @GetMapping("api/v1/login/kakao/callback")
+    @GetMapping("/api/v1/login/kakao/callback")
     public ResponseEntity<ApiResult> kakaoCallback(@RequestParam("code") String code) {
 
         Map<String, String> tokens = kakaoLoginService.kakaoOauth(code);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + tokens.get("accessToken"));
+        headers.add("Authorization", tokens.get("accessToken"));
         headers.add("Refresh-Token", tokens.get("refreshToken"));
 
         return ResponseEntity.ok()
