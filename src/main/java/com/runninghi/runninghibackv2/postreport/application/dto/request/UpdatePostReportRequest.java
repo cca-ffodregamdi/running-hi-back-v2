7877b992dbc479351a163352fe1d324642279a54
hodin <hodin030@gmail.com>
2024-04-01 23:45:44 +0900

    [#128] refactor: 게시글 신고 수정 request 변경
    -1. 수락/거절 여부 boolean 값으로 받도록 변경

--- src/main/java/com/runninghi/runninghibackv2/postreport/application/dto/request/UpdatePostReportRequest.java
+++ src/main/java/com/runninghi/runninghibackv2/postreport/application/dto/request/UpdatePostReportRequest.java
@@ -1,8 +1,5 @@
 package com.runninghi.runninghibackv2.postreport.application.dto.request;
 
-import com.runninghi.runninghibackv2.common.enumtype.ProcessingStatus;
-
 public record UpdatePostReportRequest(
-        ProcessingStatus status,
-        boolean reportedPostDeleted
+        boolean isAccepted
 ) {}
