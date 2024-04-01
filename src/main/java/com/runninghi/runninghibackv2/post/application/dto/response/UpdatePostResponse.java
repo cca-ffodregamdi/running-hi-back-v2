147b03b7cfffb59648040c90b2909a1dc21d4295
Jungin <jungin.dylan@gmail.com>
2024-04-01 14:46:58 +0900

    [#129]Setting: 24.04.01 Swagger Schema 설정

--- src/main/java/com/runninghi/runninghibackv2/post/application/dto/response/UpdatePostResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/post/application/dto/response/UpdatePostResponse.java
@@ -1,9 +1,12 @@
 package com.runninghi.runninghibackv2.post.application.dto.response;
 
 import com.runninghi.runninghibackv2.post.domain.aggregate.entity.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UpdatePostResponse(
+        @Schema(description = "게시글 제목", example = "제목 예시")
         String postTitle,
+        @Schema(description = "게시글 내용", example = "게시글 내용 예시입니다.")
         String postContent
 
 ) {
