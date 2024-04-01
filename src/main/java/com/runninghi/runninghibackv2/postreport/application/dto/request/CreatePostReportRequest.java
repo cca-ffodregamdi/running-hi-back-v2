21474f25f045547400d024297f42cf6d256bbdb7
hodin <hodin030@gmail.com>
2024-04-02 04:19:33 +0900

    [#136] feat: 게시글 신고 Swagger 적용

--- src/main/java/com/runninghi/runninghibackv2/postreport/application/dto/request/CreatePostReportRequest.java
+++ src/main/java/com/runninghi/runninghibackv2/postreport/application/dto/request/CreatePostReportRequest.java
@@ -1,10 +1,14 @@
 package com.runninghi.runninghibackv2.postreport.application.dto.request;
 
 import com.runninghi.runninghibackv2.common.enumtype.ReportCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CreatePostReportRequest(
+        @Schema(description = "신고 유형", example = "SPAM")
         ReportCategory category,
+        @Schema(description = "기타 신고 사유", example = "공백 제외 10자 이상의 사유")
         String content,
+        @Schema(description = "신고된 게시글 번호", example = "1")
         Long reportedPostNo
 )
 {}
