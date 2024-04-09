f3a3a75e22734bc562de5cb530d288206f25f8f6
SUI <numerical43@daum.net>
2024-04-09 15:21:45 +0900

    refactoring : ApiResult의 Object 타입을 제네릭으로 변경

--- src/main/java/com/runninghi/runninghibackv2/common/response/ApiResult.java
+++ src/main/java/com/runninghi/runninghibackv2/common/response/ApiResult.java
@@ -1,23 +1,29 @@
 package com.runninghi.runninghibackv2.common.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-public record ApiResult(
+public record ApiResult<T>(
+        @Schema(description = "응답 시간", example = "2024-03-27T14:20:52.026425")
         LocalDateTime timeStamp,
+        @Schema(description = "응답 상태", example = "OK")
         HttpStatus status,
+        @Schema(description = "응답 메시지", example = "성공 응답 메시지 예시")
         String message,
-        Object data
+        @Schema(description = "응답 데이터")
+        T data
 ) {
 
-    public static ApiResult success(String message, Object data) {
-        return new ApiResult(LocalDateTime.now(), HttpStatus.OK, message, data);
+    public static <T> ApiResult<T> success(String message, T data) {
+        return new ApiResult<>(LocalDateTime.now(), HttpStatus.OK, message, data);
     }
 
-    public static ApiResult error(ErrorCode errorCode) {
+    public static <T> ApiResult<T> error(ErrorCode errorCode) {
         String errorMessage = errorCode.getCode() + " : " + errorCode.getMessage();
-        return new ApiResult(LocalDateTime.now(), errorCode.getStatus(), errorMessage, null);
+        return new ApiResult<>(LocalDateTime.now(), errorCode.getStatus(), errorMessage, null);
     }
 
 }
+
