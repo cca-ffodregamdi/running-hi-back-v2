ce67b49db7af411c23d6cb2af20e6ae03313f363
hodin <hodin030@gmail.com>
2024-04-20 20:14:54 +0900

    [#165] Score 랭킹 response DTO 생성

---
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/score/GetRankingResponse.java
@@ -0,0 +1,8 @@
+package com.runninghi.runninghibackv2.application.dto.score;
+
+public interface GetRankingResponse {
+    Long getScoreNo();
+    float getDistance();
+    String getNickname();
+    int getRank();
+}
