8a38bb93809336ad3e91f07d9d9a1bc704812468
Jungin <jungin.dylan@gmail.com>
2024-04-22 08:57:26 +0900

    temp

--- src/main/java/com/runninghi/runninghibackv2/application/controller/PostController.java
+++ src/main/java/com/runninghi/runninghibackv2/application/controller/PostController.java
@@ -11,6 +11,7 @@ import com.runninghi.runninghibackv2.application.dto.post.response.GetAllPostsRe
 import com.runninghi.runninghibackv2.application.dto.post.response.GetPostResponse;
 import com.runninghi.runninghibackv2.application.dto.post.response.UpdatePostResponse;
 import com.runninghi.runninghibackv2.application.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -45,6 +46,7 @@ public class PostController {
     private static final String DELETE_RESPONSE_MESSAGE = "성공적으로 삭제되었습니다.";
 
     @GetMapping
+    @Operation(summary = "게시글 리스트 조회", description = "게시글 전체 리스트를 조회합니다. 키워드로 필터링 가능")
     public ResponseEntity<ApiResult<Page<GetAllPostsResponse>>> getAllPosts(@ModelAttribute PostKeywordCriteria criteria) {
 
         Pageable pageable = PageRequest.of(criteria.page(), criteria.size());
@@ -55,6 +57,7 @@ public class PostController {
     }
 
     @PostMapping
+    @Operation(summary = "게시글 작성", description = "러닝 기록과 게시글 작성합니다.")
     public ResponseEntity<ApiResult<CreatePostResponse>> createRecordAndPost(@RequestHeader("Authorization") String bearerToken,
                                     @RequestPart("title") String postTitle,
                                     @RequestPart("content") String postContent,
@@ -73,6 +76,7 @@ public class PostController {
     }
 
     @PostMapping("/only-record")
+    @Operation(summary = "기록만 저장", description = "게시글 공유 없이 기록만 저장합니다.")
     public ResponseEntity<ApiResult<CreatePostResponse>> createRecord(@RequestHeader("Authorization") String bearerToken,
                                                          @RequestPart("location") String locationName,
                                                          @RequestPart("gpx") MultipartFile gpxFile) throws ParserConfigurationException, IOException, SAXException {
@@ -88,6 +92,7 @@ public class PostController {
     }
 
     @PutMapping("/{postNo}")
+    @Operation(summary = "게시글 수정", description = "공유된 게시글 제목/내용을 수정합니다.")
     public ResponseEntity<ApiResult<UpdatePostResponse>> updatePost(@RequestHeader(name = "Authorization") String bearerToken,
                                                 @PathVariable Long postNo, @RequestBody UpdatePostRequest request) {
 
@@ -99,6 +104,7 @@ public class PostController {
     }
 
     @GetMapping("/{postNo}")
+    @Operation(summary = "게시글 상세보기", description = "게시글 클릭시 상세보기 가능합니다.")
     public ResponseEntity<ApiResult<GetPostResponse>> getPost(@PathVariable Long postNo) {
 
         GetPostResponse response = postService.getPostByPostNo(postNo);
@@ -107,6 +113,7 @@ public class PostController {
     }
 
     @DeleteMapping("/{postNo}")
+    @Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
     public ResponseEntity<ApiResult<Void>> deletePost(@RequestHeader(name = "Authorization") String bearerToken,
                                                 @PathVariable Long postNo) {
 
@@ -119,6 +126,7 @@ public class PostController {
 
     @HasAccess
     @GetMapping(value = "/reported")
+    @Operation(summary = "신고된 게시글 조회", description = "신고된 게시글만 볼 수 있습니다.")
     public ResponseEntity<ApiResult<Page<GetAllPostsResponse>>> getReportedPostList(@RequestParam(defaultValue = "0") @PositiveOrZero int page,
                                                          @RequestParam(defaultValue = "10") @Positive int size,
                                                          @RequestParam(defaultValue = "desc") @Pattern(regexp = "asc|desc") String sort) {
