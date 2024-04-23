6cd1a4058353aff8d29091e38ace20bbbf3e313d
SUI <numerical43@daum.net>
2024-04-23 17:37:35 +0900

    [#179] feat : Scheduling에 사용하는 deleteAll 메서드 수정

--- src/main/java/com/runninghi/runninghibackv2/domain/repository/AlarmRepository.java
+++ src/main/java/com/runninghi/runninghibackv2/domain/repository/AlarmRepository.java
@@ -1,11 +1,10 @@
 package com.runninghi.runninghibackv2.domain.repository;
 
 import com.runninghi.runninghibackv2.domain.entity.Alarm;
-import com.runninghi.runninghibackv2.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
-    void deleteAllByMember(Member member);
+    void deleteAllByMember_MemberNo(Long memberNo);
 
 }
