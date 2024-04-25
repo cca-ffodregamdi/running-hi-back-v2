adad9e6a6f720ad197bed8c730d596b7662c113d
UncleSubak <kko6369@naver.com>
2024-04-25 16:01:31 +0900

    [#193] Refactor :  DataSource 빈 충돌 방지

--- src/main/java/com/runninghi/runninghibackv2/config/AWSS3Config.java
+++ src/main/java/com/runninghi/runninghibackv2/config/AWSS3Config.java
@@ -5,11 +5,15 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import javax.sql.DataSource;
+
 @Configuration
+@ConditionalOnMissingBean(DataSource.class)
 public class AWSS3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
