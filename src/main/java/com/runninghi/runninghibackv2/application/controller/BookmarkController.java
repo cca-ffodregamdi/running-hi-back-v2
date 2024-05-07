7c088e1bc90289cc94a22e4565f5032890480a15
UncleSubak <kko6369@naver.com>
2024-05-07 17:37:19 +0900

    [#206] Refactor : 북마크 도메인 스웨거 상세 설정 추가

--- src/main/java/com/runninghi/runninghibackv2/application/controller/BookmarkController.java
+++ src/main/java/com/runninghi/runninghibackv2/application/controller/BookmarkController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,14 +43,14 @@ public class BookmarkController {
      * @apiNote 이 메서드를 사용하기 위해서는 요청 헤더에 유효한 Bearer 토큰이 포함되어야 합니다.
      *          토큰이 유효하지 않거나, 토큰에 해당하는 사용자가 존재하지 않을 경우 접근이 거부됩니다.
      */
-    @GetMapping()
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
             summary = "북마크된 게시물 리스트 조회",
             description = "사용자의 북마크된 게시글들을 조회합니다. <br /> 사용자의 북마크 게시물의 리스트를 반환합니다. ",
-            parameters = @Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "사용자 인증을 위한 Bearer 토큰", required = true),
             responses = @ApiResponse(responseCode = "200", description = GET_RESPONSE_MESSAGE)
     )
-    public ResponseEntity<ApiResult<List<BookmarkedPostListResponse>>> getBookmarkedPostList(@RequestHeader(name = "Authorization") String bearerToken) {
+    public ResponseEntity<ApiResult<List<BookmarkedPostListResponse>>> getBookmarkedPostList(@Parameter(description = "사용자 인증을 위한 Bearer 토큰")
+                                                                                             @RequestHeader(name = "Authorization") String bearerToken) {
 
         AccessTokenInfo memberInfo = jwtTokenProvider.getMemberInfoByBearerToken(bearerToken);
         List<BookmarkedPostListResponse> bookmarkedPostList = bookmarkService.getBookmarkedPostList(memberInfo.memberNo());
@@ -57,14 +58,13 @@ public class BookmarkController {
         return ResponseEntity.ok().body(ApiResult.success(GET_RESPONSE_MESSAGE, bookmarkedPostList));
     }
 
-    @PostMapping()
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
             summary = "북마크 생성",
             description = "특정 게시물을 북마크합니다. <br /> 사용자 요청으로 '사용자 번호'와 '게시글 번호'를 입력 받아 북마크 정보를 저장하고 저장 정보를 반환합니다.",
-            parameters = @Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "사용자 인증을 위한 Bearer 토큰", required = true),
             responses = @ApiResponse(responseCode = "201", description = POST_RESPONSE_MESSAGE)
     )
-    public ResponseEntity<ApiResult<CreateBookmarkResponse>> createBookmark (@RequestHeader("Authorization") String bearerToken,
+    public ResponseEntity<ApiResult<CreateBookmarkResponse>> createBookmark (@Parameter(description = "사용자 인증을 위한 Bearer 토큰") @RequestHeader("Authorization") String bearerToken,
                                                                              @RequestBody(required = true) @Schema(description = "post key 값", example = "{\"postNo\" : 1}") Map<String, Long> body) {
 
         AccessTokenInfo accessTokenInfo = jwtTokenProvider.getMemberInfoByBearerToken(bearerToken);
@@ -74,19 +74,14 @@ public class BookmarkController {
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResult.success(POST_RESPONSE_MESSAGE, response));
     }
 
-    @DeleteMapping(value = "/{postNo}")
+    @DeleteMapping(value = "/{postNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
             summary = "북마크 취소",
             description = "특정 게시물의 북마크를 취소합니다.",
-            parameters = {
-                    @Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "사용자 인증을 위한 Bearer 토큰", required = true),
-                    @Parameter(in = ParameterIn.PATH, name = "postNo", description = "북마크 취소할 특정 게시물 번호", required = true)
-            },
             responses = @ApiResponse(responseCode = "204", description = DELETE_RESPONSE_MESSAGE)
-
     )
-    public ResponseEntity<ApiResult> deleteBookmark (@RequestHeader("Authorization") String bearerToken,
-                                                     @PathVariable(name = "postNo") Long postNo) {
+    public ResponseEntity<ApiResult> deleteBookmark (@Parameter(description = "사용자 인증을 위한 Bearer 토큰") @RequestHeader("Authorization") String bearerToken,
+                                                     @Parameter(description = "북마크 취소할 특정 게시물 번호") @PathVariable(name = "postNo") Long postNo) {
 
         AccessTokenInfo accessTokenInfo = jwtTokenProvider.getMemberInfoByBearerToken(bearerToken);
         bookmarkService.deleteBookmark(accessTokenInfo.memberNo(), postNo);
