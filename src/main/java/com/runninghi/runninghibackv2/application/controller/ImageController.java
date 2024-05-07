25ac0cb9cf6ef0471a3ba3969c3818db5e005da6
UncleSubak <kko6369@naver.com>
2024-05-07 17:32:08 +0900

    [#206] Refactor : 이미지 도메인 스웨거 상세 설정 추가

--- src/main/java/com/runninghi/runninghibackv2/application/controller/ImageController.java
+++ src/main/java/com/runninghi/runninghibackv2/application/controller/ImageController.java
@@ -7,9 +7,17 @@ import com.runninghi.runninghibackv2.auth.jwt.JwtTokenProvider;
 import com.runninghi.runninghibackv2.common.dto.AccessTokenInfo;
 import com.runninghi.runninghibackv2.common.response.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,18 +35,28 @@ public class ImageController {
 
     private final ImageService imageService;
 
-    @Operation(summary = "이미지 업로드 요청", description = "이미지를 전달받아 업로드합니다. <br /> 이미지 개수 제한은 6개이며, 리스트 형태(MultipartFile)로 전달 받아 Storage에 저장하고 url을 반환합니다.")
-    @PostMapping()
-    public ResponseEntity<ApiResult<List<CreateImageResponse>>> saveImages(@RequestHeader("Authorization") String token,
-                                                                           @RequestPart("images") List<MultipartFile> imageFiles) {
+    private final static String UPLOAD_IMAGE_RESPONSE_MESSAGE = "이미지 업로드 완료";
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(
+            summary = "이미지 업로드 요청",
+            description = "이미지를 전달받아 업로드합니다. <br /> 이미지 개수 제한은 6개이며, 리스트 형태(MultipartFile)로 전달 받아 Storage에 저장하고 url을 반환합니다.",
+            responses = @ApiResponse(responseCode = "200", description = UPLOAD_IMAGE_RESPONSE_MESSAGE)
+    )
+    public ResponseEntity<ApiResult<List<CreateImageResponse>>> saveImages(@Parameter(description = "사용자 인증을 위한 BearerToken")
+                                                                           @RequestHeader("Authorization") String token,
+                                                                           @Parameter(description = "피드에 업로드할 이미지 리스트")
+                                                                           @RequestPart("images") @NotBlank List<MultipartFile> imageFiles) {
         AccessTokenInfo memberInfo = jwtTokenProvider.getMemberInfoByBearerToken(token);
         List<CreateImageResponse> imageResponseList = imageService.saveImages(imageFiles, memberInfo.memberNo());
-        return ResponseEntity.ok().body(ApiResult.success("업로드 완료", imageResponseList));
+        return ResponseEntity.ok().body(ApiResult.success(UPLOAD_IMAGE_RESPONSE_MESSAGE, imageResponseList));
     }
 
-    @Operation(summary = "이미지 다운로드", description = "이미지를 다운로드 요청합니다. <br /> 보내진 byte 형태의 파일을 반환하여 다운로드 받게 합니다.")
-    @GetMapping("download")
-    public ResponseEntity<byte[]> downloadImage(@RequestParam(value = "filename") String fileName) throws IOException {
+    @GetMapping(value = "download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @Operation(summary = "이미지 다운로드", description = "이미지를 다운로드 요청합니다. <br /> 보내진 byte 형태의 파일을 반환하여 다운로드 받게 합니다.",
+            responses = @ApiResponse(responseCode = "200", description = "이미지 다운로드 성공")
+    )
+    public ResponseEntity<byte[]> downloadImage(@Parameter(description = "다운로드 받을 파일 명(경로 포함)")@RequestParam(value = "fileName") @NotBlank String fileName) throws IOException {
 
         DownloadImageResponse response = imageService.downloadImage(fileName);
         return ResponseEntity.ok()
