fb7149113ee978f17712ad3d21ed56876f0032b5
Jungin <jungin.dylan@gmail.com>
2024-05-10 15:03:41 +0900

    Refactor: 24.05.10 #229

--- src/main/java/com/runninghi/runninghibackv2/application/controller/PostController.java
+++ src/main/java/com/runninghi/runninghibackv2/application/controller/PostController.java
@@ -20,11 +20,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
+import java.io.*;
 
 @Tag(name = "게시글/러닝코스 API", description = "게시글 작성, 조회, 수정, 삭제와 해당 게시글의 러닝코스 데이터를 조회하는 API 입니다.")
 @RestController
@@ -40,7 +39,7 @@ public class PostController {
     private static final String UPDATE_RESPONSE_MESSAGE = "성공적으로 수정되었습니다.";
     private static final String DELETE_RESPONSE_MESSAGE = "성공적으로 삭제되었습니다.";
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "게시글 리스트 조회", description = "게시글 전체 리스트를 조회합니다.\n 특정 키워드들로 필터링이 가능합니다.")
     public ResponseEntity<ApiResult<Page<GetAllPostsResponse>>> getAllPosts(@ModelAttribute PostKeywordCriteria criteria) {
 
@@ -60,7 +59,7 @@ public class PostController {
 //        return ResponseEntity.ok(ApiResult.success( GET_MAPPING_RESPONSE_MESSAGE, response));
 //    }
 
-    @GetMapping("/coordinate/{postNo}")
+    @GetMapping(value = "/coordinate/{postNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "GPX 경도, 위도 조회", description = " '나도 이 코스 달리기' 선택시 반환되는 데이터입니다. \n 지도 상 표기를 위해 경도-위도 값이 튜플 형태로 반환됩니다.")
     public ResponseEntity<ApiResult<GpxDataResponse>> getGPXData(@PathVariable Long postNo) throws ParserConfigurationException, IOException, SAXException {
 
@@ -69,20 +68,19 @@ public class PostController {
         return ResponseEntity.ok(ApiResult.success(GET_MAPPING_RESPONSE_MESSAGE, response));
     }
 
-
-    @PostMapping(value = "/gpx", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/gpx", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "러닝 데이터 저장", description = "러닝이 끝난 직후 gpx 파일을 저장하고 데이터 (거리, 속도, 시간, 등) 을 반환합니다. ")
     public ResponseEntity<ApiResult<CreateRecordResponse>> createRecordAndPost(@RequestHeader("Authorization") String bearerToken,
-                                                                               @RequestPart("gpx") MultipartFile gpxFile) throws ParserConfigurationException, IOException, SAXException {
+                                                                               @RequestPart("gpx") String gpxFile) throws Exception {
 
         AccessTokenInfo memberInfo = jwtTokenProvider.getMemberInfoByBearerToken(bearerToken);
 
-        CreateRecordResponse response = postService.createRecord(memberInfo.memberNo(), gpxFile.getResource());
+        CreateRecordResponse response = postService.createRecord(memberInfo.memberNo(), gpxFile);
 
         return ResponseEntity.ok(ApiResult.success(CREATE_RESPONSE_MESSAGE, response));
     }
 
-    @PutMapping
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "게시글 공유", description = "러닝 데이터를 게시글로 공유합니다. \n 게시글 제목, 내용, 사진url을 추가로 받습니다.")
     public ResponseEntity<ApiResult<CreatePostResponse>> createRecord(@RequestHeader("Authorization") String bearerToken,
                                                                         @RequestBody CreatePostRequest request) {
@@ -94,7 +92,7 @@ public class PostController {
         return ResponseEntity.ok(ApiResult.success(CREATE_RESPONSE_MESSAGE, response));
     }
 
-    @PutMapping("/{postNo}")
+    @PutMapping(value = "/{postNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "게시글 수정", description = "공유된 게시글의 제목/내용을 수정합니다. \n 러닝기록은 수정이 불가능합니다.")
     public ResponseEntity<ApiResult<UpdatePostResponse>> updatePost(@RequestHeader(name = "Authorization") String bearerToken,
                                                                     @PathVariable Long postNo, @RequestBody UpdatePostRequest request) {
@@ -106,7 +104,7 @@ public class PostController {
         return ResponseEntity.ok(ApiResult.success(UPDATE_RESPONSE_MESSAGE, response));
     }
 
-    @DeleteMapping("/{postNo}")
+    @DeleteMapping(value = "/{postNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
     public ResponseEntity<ApiResult<DeletePostResponse>> deletePost(@RequestHeader(name = "Authorization") String bearerToken,
                                                       @PathVariable Long postNo) {
@@ -119,7 +117,7 @@ public class PostController {
     }
 
     @HasAccess
-    @GetMapping(value = "/reported")
+    @GetMapping(value = "/reported", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "신고된 게시글 조회", description = "신고된 게시글만 볼 수 있습니다.")
     public ResponseEntity<ApiResult<Page<GetAllPostsResponse>>> getReportedPostList(@RequestParam(defaultValue = "0") @PositiveOrZero int page,
                                                                                     @RequestParam(defaultValue = "10") @Positive int size,
