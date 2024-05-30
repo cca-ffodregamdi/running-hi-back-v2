8bd10ac9338372baa39f0e0d6cd558c04bfd49f6
SUI <numerical43@daum.net>
2024-05-30 11:30:40 +0900

    [#258] feat : 작성자 추가

--- src/main/java/com/runninghi/runninghibackv2/domain/entity/Notice.java
+++ src/main/java/com/runninghi/runninghibackv2/domain/entity/Notice.java
@@ -3,6 +3,8 @@ package com.runninghi.runninghibackv2.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @Entity
@@ -19,15 +21,23 @@ public class Notice extends BaseTimeEntity {
     @Comment("공지사항 제목")
     private String title;
 
+
     @Lob
     @Column(name = "content", nullable = false)
     @Comment("공지사항 내용 (Base64 인코딩된 이미지 포함 가능)")
     private String content;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_no", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Comment("공지사항 작성자")
+    private Member noticeWriter;
+
     @Builder
-    public Notice(Long noticeNo, String title, String content) {
+    public Notice(Long noticeNo, String title, String content, Member noticeWriter) {
         this.noticeNo = noticeNo;
         this.title = title;
         this.content = content;
+        this.noticeWriter = noticeWriter;
     }
 }
