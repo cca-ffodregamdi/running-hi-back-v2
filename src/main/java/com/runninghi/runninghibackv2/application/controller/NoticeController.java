71ba05724a57549de237a3b55960b67f3eeb76ec
SUI <numerical43@daum.net>
2024-05-30 11:31:19 +0900

    [#258] feat : notice 도메인 crud 기능 추가

--- src/main/java/com/runninghi/runninghibackv2/application/controller/NoticeController.java
+++ src/main/java/com/runninghi/runninghibackv2/application/controller/NoticeController.java
@@ -2,28 +2,27 @@ package com.runninghi.runninghibackv2.application.controller;
 
 import com.runninghi.runninghibackv2.application.dto.notice.request.CreateNoticeRequest;
 import com.runninghi.runninghibackv2.application.dto.notice.request.UpdateNoticeRequest;
-import com.runninghi.runninghibackv2.application.dto.notice.response.CreateNoticeResponse;
-import com.runninghi.runninghibackv2.application.dto.notice.response.DeleteNoticeResponse;
-import com.runninghi.runninghibackv2.application.dto.notice.response.GetNoticeResponse;
-import com.runninghi.runninghibackv2.application.dto.notice.response.UpdateNoticeResponse;
+import com.runninghi.runninghibackv2.application.dto.notice.response.*;
 import com.runninghi.runninghibackv2.application.service.NoticeService;
+import com.runninghi.runninghibackv2.auth.jwt.JwtTokenProvider;
 import com.runninghi.runninghibackv2.common.annotations.HasAccess;
 import com.runninghi.runninghibackv2.common.response.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class NoticeController {
 
     private final NoticeService noticeService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     /**
      * 공지사항 생성 메소드입니다.
@@ -37,11 +36,15 @@ public class NoticeController {
             description = "새로운 공지사항을 생성합니다.",
             responses = @ApiResponse(responseCode = "200", description = "공지사항 생성 성공")
     )
-    public ResponseEntity<ApiResult<CreateNoticeResponse>> createNotice(@RequestBody CreateNoticeRequest request) {
+    public ResponseEntity<ApiResult<CreateNoticeResponse>> createNotice(
+            @RequestHeader(value = "Authorization") String token,
+            @RequestBody CreateNoticeRequest request
+    ) {
 
-        CreateNoticeResponse response = noticeService.createNotice(request);
+        Long memberNo = jwtTokenProvider.getMemberNoFromToken(token);
+        CreateNoticeResponse response = noticeService.createNotice(request, memberNo);
+
         return ResponseEntity.ok(ApiResult.success("", response));
-
     }
 
     /**
@@ -59,9 +62,11 @@ public class NoticeController {
     )
     public ResponseEntity<ApiResult<UpdateNoticeResponse>> updateNotice(
             @Parameter(description = "공지사항 ID") @PathVariable Long noticeNo,
-            @RequestBody UpdateNoticeRequest request) {
+            @RequestBody UpdateNoticeRequest request
+    ) {
 
         UpdateNoticeResponse response = noticeService.updateNotice(noticeNo, request);
+
         return ResponseEntity.ok(ApiResult.success("", response));
     }
 
@@ -70,15 +75,18 @@ public class NoticeController {
      * @param noticeNo 공지사항 ID
      * @return 조회된 공지사항 정보
      */
-    @HasAccess
     @GetMapping(value = "/api/v1/notices/{noticeNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
             summary = "공지사항 조회",
             description = "특정 공지사항을 조회합니다.",
             responses = @ApiResponse(responseCode = "200", description = "공지사항 조회 성공")
     )
-    public ResponseEntity<ApiResult<GetNoticeResponse>> getNotice(@Parameter(description = "공지사항 ID") @PathVariable Long noticeNo) {
+    public ResponseEntity<ApiResult<GetNoticeResponse>> getNotice(
+            @Parameter(description = "공지사항 ID") @PathVariable Long noticeNo
+    ) {
+
         GetNoticeResponse response = noticeService.getNotice(noticeNo);
+
         return ResponseEntity.ok(ApiResult.success("", response));
     }
 
@@ -86,15 +94,15 @@ public class NoticeController {
      * 모든 공지사항 리스트 조회 메소드입니다.
      * @return 조회된 모든 공지사항 리스트
      */
-    @HasAccess
     @GetMapping(value = "/api/v1/notices", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
             summary = "모든 공지사항 조회",
             description = "모든 공지사항 리스트를 조회합니다.",
             responses = @ApiResponse(responseCode = "200", description = "모든 공지사항 조회 성공")
     )
-    public ResponseEntity<ApiResult<List<GetNoticeResponse>>> getAllNotices() {
-        List<GetNoticeResponse> response = noticeService.getAllNotices();
+    public ResponseEntity<ApiResult<PageResponse<GetNoticeResponse>>> getAllNotices(Pageable pageable) {
+        PageResponse<GetNoticeResponse> response = noticeService.getAllNotices(pageable);
+
         return ResponseEntity.ok(ApiResult.success("", response));
     }
 
@@ -109,7 +117,10 @@ public class NoticeController {
             summary = "공지사항 삭제",
             description = "기존 공지사항을 삭제합니다.",
             responses = @ApiResponse(responseCode = "200", description = "공지사항 삭제 성공")
-    )public ResponseEntity<ApiResult<DeleteNoticeResponse>> deleteNotice(@Parameter(description = "공지사항 ID") @PathVariable Long noticeNo) {
+    )
+    public ResponseEntity<ApiResult<DeleteNoticeResponse>> deleteNotice(
+            @Parameter(description = "공지사항 ID") @PathVariable Long noticeNo
+    ) {
         DeleteNoticeResponse response = noticeService.deleteNotice(noticeNo);
         return ResponseEntity.ok(ApiResult.success("", response));
     }
---
+++ src/main/java/com/runninghi/runninghibackv2/application/service/NoticeService.java
@@ -0,0 +1,81 @@
+package com.runninghi.runninghibackv2.application.service;
+
+import com.runninghi.runninghibackv2.application.dto.notice.request.CreateNoticeRequest;
+import com.runninghi.runninghibackv2.application.dto.notice.request.UpdateNoticeRequest;
+import com.runninghi.runninghibackv2.application.dto.notice.response.*;
+import com.runninghi.runninghibackv2.domain.entity.Member;
+import com.runninghi.runninghibackv2.domain.entity.Notice;
+import com.runninghi.runninghibackv2.domain.repository.MemberRepository;
+import com.runninghi.runninghibackv2.domain.repository.NoticeRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public CreateNoticeResponse createNotice(CreateNoticeRequest request, Long memberNo) {
+        Member member = memberRepository.findById(memberNo)
+                .orElseThrow(EntityNotFoundException::new);
+
+        Notice notice = Notice.builder()
+                .title(request.title())
+                .content(request.content())
+                .noticeWriter(member)
+                .build();
+
+        noticeRepository.save(notice);
+
+        return CreateNoticeResponse.from(notice);
+    }
+
+    public UpdateNoticeResponse updateNotice(Long noticeNo, UpdateNoticeRequest request) {
+        Notice notice = noticeRepository.findById(noticeNo)
+                .orElseThrow(EntityNotFoundException::new);
+
+        Notice updateNotice = Notice.builder()
+                .title(request.title())
+                .content(request.content())
+                .noticeWriter(notice.getNoticeWriter())
+                .build();
+
+        noticeRepository.save(updateNotice);
+
+        return UpdateNoticeResponse.from(updateNotice);
+    }
+
+    @Transactional(readOnly = true)
+    public GetNoticeResponse getNotice(Long noticeNo) {
+        Notice notice = noticeRepository.findById(noticeNo)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return GetNoticeResponse.from(notice);
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<GetNoticeResponse> getAllNotices(Pageable pageable) {
+        Page<Notice> noticePage = noticeRepository.findAllNotice(pageable);
+
+        Page<GetNoticeResponse> response = noticePage.map(GetNoticeResponse::from);
+
+        return PageResponse.from(response);
+    }
+
+    @Transactional
+    public DeleteNoticeResponse deleteNotice(Long noticeNo) {
+        Notice notice = noticeRepository.findById(noticeNo)
+                .orElseThrow(EntityNotFoundException::new);
+
+        noticeRepository.delete(notice);
+
+        return DeleteNoticeResponse.from(noticeNo);
+    }
+}
