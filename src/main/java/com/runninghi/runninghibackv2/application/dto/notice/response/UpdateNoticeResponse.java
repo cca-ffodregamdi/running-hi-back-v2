04dc2d03b8b61c1576577d365537a268908ca5de
SUI <numerical43@daum.net>
2024-05-30 09:53:20 +0900

    [#258] feat : notice 관련 dto 수정

--- src/main/java/com/runninghi/runninghibackv2/application/dto/notice/response/UpdateNoticeResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/notice/response/UpdateNoticeResponse.java
@@ -1,6 +1,17 @@
 package com.runninghi.runninghibackv2.application.dto.notice.response;
 
-public record UpdateNoticeResponse(
+import com.runninghi.runninghibackv2.domain.entity.Notice;
 
+import java.time.LocalDateTime;
+
+public record UpdateNoticeResponse(
+        Long noticeNo,
+        String title,
+        String content,
+        LocalDateTime createDate
 ) {
+    public static UpdateNoticeResponse from(Notice notice) {
+        return new UpdateNoticeResponse(notice.getNoticeNo(), notice.getTitle(),
+                notice.getContent(), notice.getCreateDate());
+    }
 }
