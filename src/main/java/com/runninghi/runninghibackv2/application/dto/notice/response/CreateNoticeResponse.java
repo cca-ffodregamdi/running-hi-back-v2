854ad25b966bcef6da6bc844d44eeb13096a712d
SUI <numerical43@daum.net>
2024-05-30 09:53:20 +0900

    [#258] feat : notice 관련 dto 수정

--- src/main/java/com/runninghi/runninghibackv2/application/dto/notice/response/CreateNoticeResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/notice/response/CreateNoticeResponse.java
@@ -1,6 +1,18 @@
 package com.runninghi.runninghibackv2.application.dto.notice.response;
 
+import com.runninghi.runninghibackv2.domain.entity.Notice;
+
+import java.time.LocalDateTime;
+
 public record CreateNoticeResponse(
+        Long noticeNo,
+        String title,
+        String content,
+        LocalDateTime createDate
 
 ) {
+    public static CreateNoticeResponse from(Notice notice) {
+        return new CreateNoticeResponse(notice.getNoticeNo(), notice.getTitle(),
+                notice.getContent(), notice.getCreateDate());
+    }
 }
--- src/main/java/com/runninghi/runninghibackv2/application/dto/notice/response/GetNoticeResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/notice/response/GetNoticeResponse.java
@@ -1,4 +1,17 @@
 package com.runninghi.runninghibackv2.application.dto.notice.response;
 
-public record GetNoticeResponse() {
+import com.runninghi.runninghibackv2.domain.entity.Notice;
+
+import java.time.LocalDateTime;
+
+public record GetNoticeResponse(
+        Long noticeNo,
+        String title,
+        String content,
+        LocalDateTime createDate
+) {
+    public static GetNoticeResponse from(Notice notice) {
+        return new GetNoticeResponse(notice.getNoticeNo(), notice.getTitle(),
+                notice.getContent(), notice.getCreateDate());
+    }
 }
