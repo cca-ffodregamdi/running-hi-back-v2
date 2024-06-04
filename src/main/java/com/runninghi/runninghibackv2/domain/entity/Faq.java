29d421deac05b2266cffc17128a692c8eb424a6e
SUI <numerical43@daum.net>
2024-06-04 16:39:20 +0900

    [#272] feat : FAQ Entity 생성

---
+++ src/main/java/com/runninghi/runninghibackv2/domain/entity/Faq.java
@@ -0,0 +1,37 @@
+package com.runninghi.runninghibackv2.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Entity
+@Table(name = "TBL_FAQ")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Faq {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "faq_no")
+    private Long faqNo;
+
+    @Column(name = "question", length = 100, nullable = false)
+    @Comment("faq 질문")
+    private String question;
+
+    @Column(name = "answer", nullable = false)
+    @Comment("faq 답변")
+    private String answer;
+
+    @Builder
+    public Faq(Long faqNo, String question, String answer) {
+        this.faqNo = faqNo;
+        this.question = question;
+        this.answer = answer;
+    }
+
+
+}
