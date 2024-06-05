e91e79b3aeb13e8ae4c60be4461fdd5af5f9efc6
SUI <numerical43@daum.net>
2024-06-05 13:45:35 +0900

    [#272] style : faq dto 스웨거 설정 추가

--- src/main/java/com/runninghi/runninghibackv2/application/dto/faq/request/CreateFaqRequest.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/faq/request/CreateFaqRequest.java
@@ -1,7 +1,12 @@
 package com.runninghi.runninghibackv2.application.dto.faq.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "FAQ 작성 요청")
 public record CreateFaqRequest(
+        @Schema(description = "질문", example = "RunningHi가 무엇인가요?")
         String question,
+        @Schema(description = "답변", example = "RunningHi는 러닝 앱입니다.")
         String answer
 ) {
 }
--- src/main/java/com/runninghi/runninghibackv2/application/dto/faq/response/CreateFaqResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/faq/response/CreateFaqResponse.java
@@ -1,10 +1,15 @@
 package com.runninghi.runninghibackv2.application.dto.faq.response;
 
 import com.runninghi.runninghibackv2.domain.entity.Faq;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "FAQ 생성 응답")
 public record CreateFaqResponse(
+        @Schema(description = "FAQ 번호", example = "1")
         Long faqNo,
+        @Schema(description = "질문", example = "RunningHi가 무엇인가요?")
         String question,
+        @Schema(description = "답변", example = "RunningHi는 러닝 앱입니다.")
         String answer
 ) {
     public static CreateFaqResponse from(Faq faq) {
