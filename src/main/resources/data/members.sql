8ddcc21e7017f1cb9c465f59e8dcd752e0122174
Jungin <jungin.dylan@gmail.com>
2024-06-07 16:07:28 +0900

    hotfix: Member insert 문 수정

--- src/main/resources/data/members.sql
+++ src/main/resources/data/members.sql
@@ -1,4 +1,5 @@
-INSERT INTO tbl_member (deactivate_date, alarm_consent, name, role, is_active, total_distance, total_kcal, distance_to_next_level, level)
+INSERT INTO tbl_member (deactivate_date, alarm_consent, name, role, is_active, total_distance, total_kcal, distance_to_next_level, level, report_cnt, is_blacklisted)
 VALUES
-    ('2024-06-07 10:00:00', true, 'name', 'USER', false, 1000, 5000, 30, 1),
-    (NOW() - INTERVAL 15 DAY, true, 'nyam', 'USER', true, 1000, 5000, 30, 1);
+    (null, true, 'name', 'USER', false, 1000, 5000, 30, 1, 0, false),
+    (null, true, 'name2', 'USER', false, 1000, 5000, 30, 1, 0, false),
+    (NOW() - INTERVAL 15 DAY, true, 'nyam', 'USER', true, 1000, 5000, 30, 1, 0, false);
