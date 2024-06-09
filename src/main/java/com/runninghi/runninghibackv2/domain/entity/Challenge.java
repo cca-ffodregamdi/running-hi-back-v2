b60014e8ec64abf6a1a7cc4adc3add2d12addec1
hodin <hodin030@gmail.com>
2024-06-09 23:49:26 +0900

    refactor: 챌린지 엔티티 변경

--- src/main/java/com/runninghi/runninghibackv2/domain/entity/Challenge.java
+++ src/main/java/com/runninghi/runninghibackv2/domain/entity/Challenge.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "TBL_CHALLENGE")
-public class Challenge {
+public class Challenge extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,9 +30,16 @@ public class Challenge {
     @Comment("챌린지 상세정보")
     private String content;
 
+    //@Comment("챌린지 타입")
+
     @NotNull
     @Comment("목표 거리")
     private float distance;
+
+    @Comment("목표 속도")
+    private float speed;
+
+    //@Comment("목표 출석 현황")
 
     @Comment("챌린지 시작일자")
     private LocalDateTime startDate;
@@ -40,23 +47,31 @@ public class Challenge {
     @Comment("챌린지 종료일자")
     private LocalDateTime endDate;
 
-    @Comment("달성 보상")
-    private String reward;  // 보상 종류, 타입 수정
+    @Comment("누적 달린 시간")
+    private float runningTime;
+
+    @Comment("누적 소모 칼로리")
+    private float kcal;
+
+    @Comment("누적 평균 페이스 (분/km)")
+    private float meanPace;
 
     @OneToMany(mappedBy = "challenge")
-    @Comment("챌린지에 참여한 멤버")
+    @Comment("챌린지에 참여한 멤버 리스트")
     private List<MemberChallenge> participants;
 
     @Builder
     public Challenge(Long challengeNo, String title, String content, float distance, LocalDateTime startDate,
-                     LocalDateTime endDate, String reward, List<MemberChallenge> participants) {
+                     LocalDateTime endDate, float runningTime, float kcal, float meanPace, List<MemberChallenge> participants) {
         this.challengeNo = challengeNo;
         this.title = title;
         this.content = content;
         this.distance = distance;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.reward = reward;
+        this.runningTime = runningTime;
+        this.kcal = kcal;
+        this.meanPace = meanPace;
         this.participants = participants;
     }
 
@@ -66,6 +81,5 @@ public class Challenge {
         this.distance = request.distance();
         this.startDate = request.startDate();
         this.endDate = request.endDate();
-        this.reward = request.reward();
     }
 }
