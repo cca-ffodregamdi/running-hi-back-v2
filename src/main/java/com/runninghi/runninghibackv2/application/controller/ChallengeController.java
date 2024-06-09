1c52acf579d12793b9fcfe5635f1bf73ba9ed796
hodin <hodin030@gmail.com>
2024-06-10 02:54:06 +0900

    [#297] feat: 멤버챌린지 저장, 전체 조회, 상세 조회 API 구현

--- src/main/java/com/runninghi/runninghibackv2/application/controller/ChallengeController.java
+++ src/main/java/com/runninghi/runninghibackv2/application/controller/ChallengeController.java
@@ -1,12 +1,14 @@
 package com.runninghi.runninghibackv2.application.controller;
 
+import com.runninghi.runninghibackv2.application.dto.memberchallenge.request.CreateMyChallengeRequest;
 import com.runninghi.runninghibackv2.application.dto.challenge.request.UpdateChallengeRequest;
-import com.runninghi.runninghibackv2.application.dto.challenge.response.GetChallengeResponse;
-import com.runninghi.runninghibackv2.application.dto.challenge.response.UpdateChallengeResponse;
+import com.runninghi.runninghibackv2.application.dto.challenge.response.*;
+import com.runninghi.runninghibackv2.application.dto.memberchallenge.response.CreateMyChallengeResponse;
+import com.runninghi.runninghibackv2.application.dto.memberchallenge.response.GetMyChallengeResponse;
 import com.runninghi.runninghibackv2.application.service.ChallengeService;
 import com.runninghi.runninghibackv2.application.dto.challenge.request.CreateChallengeRequest;
-import com.runninghi.runninghibackv2.application.dto.challenge.response.CreateChallengeResponse;
-import com.runninghi.runninghibackv2.application.dto.challenge.response.DeleteChallengeResponse;
+import com.runninghi.runninghibackv2.application.service.MyChallengeService;
+import com.runninghi.runninghibackv2.auth.jwt.JwtTokenProvider;
 import com.runninghi.runninghibackv2.common.response.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,8 @@ import java.util.List;
 public class ChallengeController {
 
     private final ChallengeService challengeService;
+    private final MyChallengeService myChallengeService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping()
     public ResponseEntity<ApiResult<CreateChallengeResponse>> createChallenge(
@@ -62,5 +66,40 @@ public class ChallengeController {
         DeleteChallengeResponse response = challengeService.deleteChallenge(challengeNo);
 
         return ResponseEntity.ok(ApiResult.success("챌린지 삭제 성공", response));
+    }
+
+    @PostMapping("/my-challenge")
+    public ResponseEntity<ApiResult<CreateMyChallengeResponse>> createMyChallenge(
+            @RequestBody CreateMyChallengeRequest request,
+            @RequestHeader(name = "Authorization") String bearerToken) {
+
+        Long memberNo = jwtTokenProvider.getMemberNoFromToken(bearerToken);
+
+        CreateMyChallengeResponse response = myChallengeService.createMyChallenge(request, memberNo);
+
+        return ResponseEntity.ok(ApiResult.success("나의 챌린지 저장 성공", response));
+    }
+
+    @GetMapping("/my-challenge")
+    public ResponseEntity<ApiResult<List<GetMyChallengeResponse>>> getAllMyChallenges(
+            @RequestHeader(name = "Authorization") String bearerToken) {
+
+        Long memberNo = jwtTokenProvider.getMemberNoFromToken(bearerToken);
+
+        List<GetMyChallengeResponse> response = myChallengeService.getAllMyChallenges(memberNo);
+
+        return ResponseEntity.ok(ApiResult.success("나의 챌린지 전체 조회 성공", response));
+    }
+
+    @GetMapping("/my-challenge/{myChallengeNo}")
+    public ResponseEntity<ApiResult<GetMyChallengeResponse>> getMyChallengeById(
+            @PathVariable Long myChallengeNo,
+            @RequestHeader(name = "Authorization") String bearerToken) {
+
+        Long memberNo = jwtTokenProvider.getMemberNoFromToken(bearerToken);
+
+        GetMyChallengeResponse response = myChallengeService.getMyChallengeById(memberNo, myChallengeNo);
+
+        return ResponseEntity.ok(ApiResult.success("나의 챌린지 상세 조회 성공", response));
     }
 }
