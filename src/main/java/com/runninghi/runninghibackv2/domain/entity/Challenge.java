1799382898de53cdac6df851bd86fd67c270df33
hodin <hodin030@gmail.com>
2024-06-10 02:31:15 +0900

    refactor: 챌린지 카테고리 enum 추가, 목표 수치 한 컬럼으로 통일

--- src/main/java/com/runninghi/runninghibackv2/domain/entity/Challenge.java
+++ src/main/java/com/runninghi/runninghibackv2/domain/entity/Challenge.java
@@ -1,6 +1,8 @@
 package com.runninghi.runninghibackv2.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.runninghi.runninghibackv2.application.dto.challenge.request.UpdateChallengeRequest;
+import com.runninghi.runninghibackv2.domain.enumtype.ChallengeCategory;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -30,16 +32,14 @@ public class Challenge extends BaseTimeEntity {
     @Comment("챌린지 상세정보")
     private String content;
 
-    //@Comment("챌린지 타입")
+    @NotNull
+    @Comment("챌린지 타입")
+    @Enumerated(value = EnumType.STRING)
+    private ChallengeCategory challengeCategory;
 
     @NotNull
-    @Comment("목표 거리")
-    private float distance;
-
-    @Comment("목표 속도")
-    private float speed;
-
-    //@Comment("목표 출석 현황")
+    @Comment("목표 수치")
+    private float targetValue;
 
     @Comment("챌린지 시작일자")
     private LocalDateTime startDate;
@@ -48,37 +48,40 @@ public class Challenge extends BaseTimeEntity {
     private LocalDateTime endDate;
 
     @Comment("누적 달린 시간")
-    private float runningTime;
+    private float totalRunningTime;
 
     @Comment("누적 소모 칼로리")
-    private float kcal;
+    private float totalKcal;
 
     @Comment("누적 평균 페이스 (분/km)")
-    private float meanPace;
+    private float totalMeanPace;
 
     @OneToMany(mappedBy = "challenge")
     @Comment("챌린지에 참여한 멤버 리스트")
     private List<MemberChallenge> participants;
 
     @Builder
-    public Challenge(Long challengeNo, String title, String content, float distance, LocalDateTime startDate,
-                     LocalDateTime endDate, float runningTime, float kcal, float meanPace, List<MemberChallenge> participants) {
+    public Challenge(Long challengeNo, String title, String content, ChallengeCategory challengeCategory,
+                     float targetValue, LocalDateTime startDate, LocalDateTime endDate,
+                     float totalRunningTime, float totalKcal, float totalMeanPace, List<MemberChallenge> participants) {
         this.challengeNo = challengeNo;
         this.title = title;
         this.content = content;
-        this.distance = distance;
+        this.challengeCategory = challengeCategory;
+        this.targetValue = targetValue;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.runningTime = runningTime;
-        this.kcal = kcal;
-        this.meanPace = meanPace;
+        this.totalRunningTime = totalRunningTime;
+        this.totalKcal = totalKcal;
+        this.totalMeanPace = totalMeanPace;
         this.participants = participants;
     }
 
     public void update(UpdateChallengeRequest request) {
         this.title = request.title();
         this.content = request.content();
-        this.distance = request.distance();
+        this.challengeCategory = request.challengeCategory();
+        this.targetValue = request.targetValue();
         this.startDate = request.startDate();
         this.endDate = request.endDate();
     }
