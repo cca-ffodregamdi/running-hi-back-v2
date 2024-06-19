16985a8946e03e3901b7a603e02758be33641ad3
hodin <hodin030@gmail.com>
2024-06-19 18:33:35 +0900

    [#332] Feature: 특정 기간동안 생성된 멤버의 post가 존재하는지 조회하는 쿼리 추가

--- src/main/java/com/runninghi/runninghibackv2/domain/repository/PostRepository.java
+++ src/main/java/com/runninghi/runninghibackv2/domain/repository/PostRepository.java
@@ -1,13 +1,17 @@
 package com.runninghi.runninghibackv2.domain.repository;
 
 import com.runninghi.runninghibackv2.domain.entity.Member;
+import com.runninghi.runninghibackv2.domain.entity.MemberChallenge;
 import com.runninghi.runninghibackv2.domain.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -16,4 +20,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllByReportCntIsGreaterThan(int reportCnt, Pageable pageable);
 
     List<Post> findAllByMember(Member deactivateMember);
+
+    Optional<Post> findFirstByMemberAndCreateDateBetween(Member member, LocalDateTime startDate, LocalDateTime endDate);
 }
