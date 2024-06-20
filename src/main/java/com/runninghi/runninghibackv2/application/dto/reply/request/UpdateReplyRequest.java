f53ca99aee9b64d3488162cc01104aa968010ebf
UncleSubak <kko6369@naver.com>
2024-06-21 01:24:11 +0900

    [#339] Refactor : 2024-06-20 수정 API validation 추가

--- src/main/java/com/runninghi/runninghibackv2/application/dto/reply/request/UpdateReplyRequest.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/reply/request/UpdateReplyRequest.java
@@ -2,6 +2,7 @@ package com.runninghi.runninghibackv2.application.dto.reply.request;
 
 import com.runninghi.runninghibackv2.domain.enumtype.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -17,6 +18,7 @@ public class UpdateReplyRequest {
     Role role;
     @Schema(hidden = true)
     Long replyNo;
+    @NotBlank(message = "수정할 댓글을 입력해주세요.")
     @Schema(description = "변경할 댓글 내용", example = "변경된 댓글 내용")
     String replyContent;
 
