0ac5bdf2b5ef0aa893e446f0e85682cf6b494932
이효진 <hodin030@gmail.com>
2024-06-24 15:48:10 +0900

    [#364] Refactor: 챌린지 상세조회 응답 DTO에 챌린지 참여자수, 랭킹 필드 추가

--- src/main/java/com/runninghi/runninghibackv2/application/dto/challenge/response/GetChallengeResponse.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/challenge/response/GetChallengeResponse.java
@@ -1,10 +1,12 @@
 package com.runninghi.runninghibackv2.application.dto.challenge.response;
 
+import com.runninghi.runninghibackv2.application.dto.memberchallenge.response.GetChallengeRankingResponse;
 import com.runninghi.runninghibackv2.domain.entity.Challenge;
 import com.runninghi.runninghibackv2.domain.enumtype.ChallengeCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record GetChallengeResponse(
         @Schema(description = "챌린지 Id", example = "1")
@@ -22,9 +24,13 @@ public record GetChallengeResponse(
         @Schema(description = "챌린지 시작일자", example = "2024-06-01T00:00:00")
         LocalDateTime startDate,
         @Schema(description = "챌린지 종료일자", example = "2024-0.6-30T00:00:00")
-        LocalDateTime endDate
+        LocalDateTime endDate,
+        @Schema(description = "챌린지 참여자 수", example = "4132")
+        int participantsCount,
+        @Schema(description = "챌린지 랭킹", example = "10")
+        List<GetChallengeRankingResponse> ranking
 ) {
-    public static GetChallengeResponse from(Challenge challenge) {
+    public static GetChallengeResponse from(Challenge challenge, List<GetChallengeRankingResponse> ranking) {
         return new GetChallengeResponse(
                 challenge.getChallengeNo(),
                 challenge.getTitle(),
@@ -33,7 +39,9 @@ public record GetChallengeResponse(
                 challenge.getImageUrl(),
                 challenge.getTargetValue(),
                 challenge.getStartDate(),
-                challenge.getEndDate()
+                challenge.getEndDate(),
+                challenge.getParticipants().size(),
+                ranking
         );
     }
 }
