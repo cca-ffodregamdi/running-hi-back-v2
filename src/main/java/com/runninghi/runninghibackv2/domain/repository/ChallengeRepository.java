0a02cb9d95670fc4df165a644c4be89b8e9f7b2d
이효진 <hodin030@gmail.com>
2024-06-24 15:50:53 +0900

    [#364] Feat: 챌린지 조회 시 활성화 상태에 따라 조회하는 기능

--- src/main/java/com/runninghi/runninghibackv2/domain/repository/ChallengeRepository.java
+++ src/main/java/com/runninghi/runninghibackv2/domain/repository/ChallengeRepository.java
@@ -4,6 +4,9 @@ import com.runninghi.runninghibackv2.domain.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+    List<Challenge> findByStatus(boolean status);
 }
