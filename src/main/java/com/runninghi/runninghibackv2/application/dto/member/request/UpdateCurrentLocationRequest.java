a0d5c26faad72e6645c2209173216147824ba2d7
SUI <numerical43@daum.net>
2024-08-04 13:48:54 +0900

    [#467] refactor : @getter 어노테이션 삭제

--- src/main/java/com/runninghi/runninghibackv2/application/dto/member/request/UpdateCurrentLocationRequest.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/member/request/UpdateCurrentLocationRequest.java
@@ -1,8 +1,5 @@
 package com.runninghi.runninghibackv2.application.dto.member.request;
 
-import lombok.Getter;
-
-@Getter
 public record UpdateCurrentLocationRequest(
         double latitude,
         double longitude
