85a46f18bf3b66aa86747615d7777cf8bcc37133
SUI <numerical43@gmail.com>
2024-08-06 21:48:03 +0900

    [#484] rename : 필요없는 파일 삭제, dto 이름 변경

--- src/main/java/com/runninghi/runninghibackv2/application/dto/member/request/AdminSignInRequest.java
+++ src/main/java/com/runninghi/runninghibackv2/application/dto/member/request/AdminSignInRequest.java
@@ -1,6 +1,6 @@
 package com.runninghi.runninghibackv2.application.dto.member.request;
 
-public record AdminLoginRequest(
+public record AdminSignInRequest(
         String account,
         String password
 ) {
--- src/main/java/com/runninghi/runninghibackv2/application/dto/member/response/AdminLoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.runninghi.runninghibackv2.application.dto.member.response;
-
-public record AdminLoginResponse(
-        Long memberNo
-) {
-    public static AdminLoginResponse from(Long memberNo) {
-        return new AdminLoginResponse(memberNo);
-    }
-}
