483c7596c21aae92af4698f40d1282048cb5d630
silverpoodle <jungin.dylan@gmail.com>
2024-08-15 16:59:45 +0900

    [#505][Feature] 더미데이터 수정

--- src/main/resources/data/members.sql
+++ src/main/resources/data/members.sql
@@ -1,9 +1,14 @@
-INSERT INTO tbl_member (deactivate_date, alarm_consent, name, role, is_active, total_distance, total_kcal, distance_to_next_level, level, report_cnt, is_blacklisted, geometry)
+INSERT INTO tbl_member (member_no, name, nickname, alarm_consent, distance_to_next_level, is_active, is_blacklisted, level, report_cnt, total_distance, total_kcal, create_date, profile_url, geometry)
 VALUES
-    (null, true, 'name', 'USER', false, 1000, 5000, 30, 1, 0, false,ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat')),
-    (null, true, 'name2', 'USER', false, 1000, 5000, 30, 1, 0, false,ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat')),
-    (NOW() - INTERVAL 15 DAY, true, 'nyam', 'USER', true, 1000, 5000, 30, 1, 0, false,ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat'));
+    (1, '유저1', '러너 1', true, 10, true, false, 0, 0, 0, 0, '2024-08-15 16:21:28', null, ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat')),
+    (2, '유저2', '러너 2', true, 10, true, false, 0, 0, 0, 0, '2024-08-15 16:21:30', null, ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat')),
+    (3, '유저3', '러너 3', true, 10, true, false, 0, 0, 0, 0, '2024-08-15 16:21:31', null, ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat')),
+    (4, '유저4', '러너 4', true, 10, true, false, 0, 0, 0, 0, '2024-08-15 16:21:34', null, ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat')),
+    (5, '유저5', '러너 5', true, 10, true, false, 0, 0, 0, 0, '2024-08-15 16:21:36', null, ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat'));
+
 
 UPDATE tbl_member
-SET geometry = ST_GeomFromText('POINT(129.602 37.1931)', 4326, 'axis-order=long-lat')
-WHERE member_no = 4;
+SET geometry = ST_GeomFromText('POINT(127.518 37.263)', 4326, 'axis-order=long-lat')
+WHERE member_no =11246;
+
+
--- src/main/resources/data/postreports.sql
+++ src/main/resources/data/postreports.sql
@@ -1,6 +1,6 @@
-INSERT INTO tbl_post_report (category, content, status, reporter_no, reported_post_no, post_content, runninghi.tbl_post_report.is_post_deleted)
+INSERT INTO tbl_post_report (category, content, status, reporter_no, reported_post_no, post_content)
 VALUES
-    ('SPAM', '스팸 게시글입니다.', 'INPROGRESS', 2, 1, (SELECT post_content FROM tbl_post WHERE post_no = 1),false),
-    ('ILLEGAL', '부적절한 내용이 포함된 게시글입니다.', 'INPROGRESS', 1, 2, (SELECT post_content FROM tbl_post WHERE post_no = 2),false),
-    ('ILLEGAL', '부적절한 내용이 포함된 게시글입니다.', 'INPROGRESS', 2, 2, (SELECT post_content FROM tbl_post WHERE post_no = 2),false);
+    ('SPAM', '스팸 게시글입니다.', 'INPROGRESS', 2, 1, (SELECT post_content FROM tbl_post WHERE post_no = 1)),
+    ('ILLEGAL', '부적절한 내용이 포함된 게시글입니다.', 'INPROGRESS', 1, 2, (SELECT post_content FROM tbl_post WHERE post_no = 2)),
+    ('ILLEGAL', '부적절한 내용이 포함된 게시글입니다.', 'INPROGRESS', 2, 2, (SELECT post_content FROM tbl_post WHERE post_no = 2));
 
